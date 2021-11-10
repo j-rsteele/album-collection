7bca7925829bcb585dd3a12be164e90d5d11ce8d
Dr. Richard Montgomery <drmontgomery2@outlook.com>
2021-11-10 15:18:47 -0500

    RichandJhovany

---
+++ backend/album-collection/Models/Album.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace album_collection.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+
+        public string title { get; set; }
+
+        //public string imageID
+
+        public string artist { get; set; }
+
+        public string review { get; set; }
+
+        public virtual List<Songs> songs { get; set; }
+    }
+}
+
+album
+id
+title
+image
+artist
+songs (either as a string to be separated, or see below)
+reviews
+record label
+
+song
+id
+title
+album
