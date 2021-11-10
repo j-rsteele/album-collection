f50a684bc6f7a068de904000867c550aee4f0e92
jfeller403 <jfeller403@gmail.com>
2021-11-09 23:42:51 -0500

    added nuget packages including newtonsoft.Json and put method in startup file

--- backend/album-collection/Startup.cs
+++ backend/album-collection/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace album_collection
 {
@@ -25,7 +26,8 @@ namespace album_collection
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            // handles circular reference error -Jesse 
+            services.AddControllers().AddNewtonsoftJson(o => o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
