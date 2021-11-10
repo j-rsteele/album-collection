1154cdcd89b284497bc9a478d26aeb5b793932f5
Jenna Steele <jsteele021891@gmail.com>
2021-11-10 15:05:25 -0500

    created artist model

---
+++ backend/album-collection/Controllers/ArtistController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace album_collection.Controllers
+{
+    public class ArtistController : Controller
+    {
+        ArtistController()
+        {
+
+        }
+
+        public IActionResult <List<Artist>> Get()
+        {
+            return _db.Artists.ToList();
+        }
+    }
+}
