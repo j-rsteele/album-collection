c840d445431a1192cf3a63f809803b4d89d98fec
jfeller403 <jfeller403@gmail.com>
2021-11-17 12:16:28 -0500

    edit almost done

--- frontend/src/js/Albums/albums.js
+++ frontend/src/js/Albums/albums.js
@@ -4,8 +4,6 @@ const title=document.getElementById("title");
 
 export default{
     displayAllAlbums,
-    viewAddAlbum,
-    SetupSaveButton
 }
 
 export function viewAddAlbum()
@@ -14,13 +12,24 @@ export function viewAddAlbum()
     content.innerHTML =
     `
         Title <input type="text" value="" id="album_title" />
-        Artist <input type="text" value="" id="album_artist" />
+        Artist <select id="listArtists"> </select>
         ImageURL <input type="text" value="" id="album_image" />
         Record Label <input type="text" value="" id="album_recordLabel" />
-    <button id="btnSaveAlbum">Save Album</button>
+        <button id="btnSaveAlbum">Save Album</button>
     `
-    SetupSaveButton()
-
+    const artistsSelectList = document.getElementById("listArtists")
+    //populates select list for artist
+    api.getRequest("https://localhost:44313/api/artist", artists =>
+        {
+            artists.forEach(artist =>
+            {
+                var option = document.createElement("option");
+                option.value = artist.id;
+                option.text = artist.name;
+                artistsSelectList.appendChild(option);
+            }); 
+        });
+    SetupSaveButton(artistsSelectList)
 }
 
 function displayAllAlbums()
@@ -33,35 +42,93 @@ function displayAllAlbums()
         content.innerHTML =
         `
         <ol>
-            ${data.map(album => "<li> Title: "+album.title+"</li>").join("")}
+            ${data.map(album => '<li>'+album.title+'</li><button class="editAlbum" albumID='+album.id+'>Edit</button>').join("")}
         </ol>
         <button id="btnAddAlbum">Add Album</button>
         `
         let btnAddAlbum = document.getElementById("btnAddAlbum");
-        btnAddAlbum.addEventListener("click", viewAddAlbum
-
-        );
-    
+        btnAddAlbum.addEventListener("click", viewAddAlbum);
+        let editButtons = Array.from(document.getElementsByClassName("editAlbum"));
+        editButtons.forEach(editButton =>
+        {
+            editButton.addEventListener("click", function()
+            {
+                let id = editButton.getAttribute("albumId");
+                viewEditAlbum(id);
+            });
+        });
     }
 
 }
 
-export function SetupSaveButton()
+export function SetupSaveButton(selectedArtist)
 {
     let btnSave = document.getElementById("btnSaveAlbum");
     btnSave.addEventListener("click", function()
     {
         let albumTitle = document.getElementById("album_title").value;
-        let albumArtist = document.getElementById("album_artist").value;
         let albumImage = document.getElementById("album_image").value;
         let albumRecordLabel = document.getElementById("album_recordLabel").value;
 
-        const Album = {
+        const Album = 
+        {
             Title: albumTitle,
-            Artist: albumArtist,
             Image: albumImage,
-            RecordLabel: albumRecordLabel
+            RecordLabel: albumRecordLabel,
+            artistId: selectedArtist.value
         }
         api.postRequest("https://localhost:44313/api/album", Album, displayAllAlbums);
     });
+}
+
+function viewEditAlbum(id)
+{
+    title.innerText = "Edit Album";
+    let selectedArtist;
+    api.getRequest("https://localhost:44313/api/album/"+id, album =>
+        {
+            content.innerHTML =
+            `
+                Title <input type="text" value="${album.title}" id="album_title" />
+                Artist <select id="listArtists"> </select>
+                ImageURL <input type="text" value="${album.image}" id="album_image" />
+                Record Label <input type="text" value="${album.RecordLabel}" id="album_recordLabel" />
+                <button id="btnSaveEdit">Save Album</button>
+            `
+            const artistsSelectList = document.getElementById("listArtists")
+            api.getRequest("https://localhost:44313/api/artist", artists =>
+            {
+                artists.forEach(artist =>
+                    {
+                        var option = document.createElement("option");
+                        option.value = artist.id;
+                        option.text = artist.name;
+                        if(artist.id==album.artistId)
+                            {option.setAttribute("selected","");
+                            selectedArtist = album.artistId
+                            }
+                        artistsSelectList.appendChild(option);
+                    });
+
+            });
+            let btnSaveEdit = document.getElementById("btnSaveEdit");
+            btnSaveEdit.addEventListener("click", function()
+            {
+                let albumTitle = document.getElementById("album_title").value;
+                let albumImage = document.getElementById("album_image").value;
+                let albumRecordLabel = document.getElementById("album_recordLabel").value;
+
+                const Album = 
+                {
+                    Title: albumTitle,
+                    Image: albumImage,
+                    recordLabel: albumRecordLabel,
+                    artistId: String(selectedArtist)
+                }
+                console.log(Album)
+                api.putRequest("https://localhost:44313/api/album/",id, Album, displayAllAlbums);
+            });
+        });
+
+    
 }
